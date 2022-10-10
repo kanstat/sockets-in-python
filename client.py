89915cf2fbd478bf046e19438b4088825c3ccf6d
kanstat <kanchan.sharma9722@gmail.com>
2022-10-10 20:39:04 +0530

    comment added

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import socket
 import threading
 
+# function for checking incoming msgs from server
+
+
 def check_mssgs_from_server(conn):
     while True:
         data_recv = conn.recv(1024).decode()
-        print("SERVER->",end="")
+        print("SERVER->", end="")
         print(data_recv)
 
 
@@ -13,9 +16,10 @@ if __name__ == "__main__":
     server_port = 7000
 
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.connect((server_host,server_port))        
+    c.connect((server_host, server_port))
     if c:
-        recv_thread = threading.Thread(target=check_mssgs_from_server,args=(c,))
+        recv_thread = threading.Thread(
+            target=check_mssgs_from_server, args=(c,))
         recv_thread.start()
     while True:
         to_send = input("YOU->")
