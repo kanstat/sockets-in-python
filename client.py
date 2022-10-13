0c3b57d17a9de5af7411a634df30c3d9a73bb152
dynstat <vs15vivek@gmail.com>
2022-10-13 22:42:10 +0530

    improved chat displays

--- client.py
+++ client.py
@@ -1,27 +1,35 @@
+# import socket module
 import socket
 import threading
 
-def check_mssgs_from_server(conn):
+# function to handle the received data from the client continuously
+def server_ne_kuch_bheja_kya(connected_server,nickname):
     while True:
-        data_recv = conn.recv(1024).decode()
-        print("SERVER->",end="")
-        print(data_recv)
+        if connected_server:
+            try:
+                server_ka_bheja_hua_mssg = connected_server.recv(1024).decode()
+                print("\33[2K",end="")
+                # print("\r"+nickname+":"+server_ka_bheja_hua_mssg+"\nCLIENT:",end=" ")
+                print(f"\rSERVER: {server_ka_bheja_hua_mssg}\nCLIENT:",end=" ")
+            except:
+                break
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+server_host_to_be_connected = "127.0.0.1"
+server_port_to_be_connected = 2005
 
-if __name__ == "__main__":
-    server_host = "127.0.0.1"
-    server_port = 7000
+c.connect((server_host_to_be_connected, server_port_to_be_connected))
+nickname = input("Enter your chat-room name: ")
+if nickname:
+    c.send(f"{nickname}".encode())
+else:
+    c.send(f"CLIENT".encode())
+    
+# Thread to handling receiving mssgs from client independently
+server_thread = threading.Thread(target=server_ne_kuch_bheja_kya, args=(c,nickname))
+server_thread.start()
 
-    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.connect((server_host,server_port))        
-    if c:
-        recv_thread = threading.Thread(target=check_mssgs_from_server,args=(c,))
-        recv_thread.start()
-        
-    while True:
-        if c:
-            to_send = input("YOU->")
-            c.send(to_send.encode())
-        else:
-            print("NOT CONNECTED",end="\r")            
-        
+while True:
+    mssg_to_send = input("\rCLIENT: ")
+    c.send(f"{mssg_to_send}".encode())
+
--- server.py
+++ server.py
@@ -1,39 +1,39 @@
+# import socket module
 import socket
 import threading
 
-
-def check_mssgs_from_client(conn):
+# function to handle the received data from the client continuously
+def client_ne_kuch_bheja_kya(connected_client,nickname):
     while True:
-        data_recv = conn.recv(1024).decode()
-        if data_recv == "exit":
-            print("Exiting Server..!!")
+        try:
+            client_ka_bheja_hua_mssg = connected_client.recv(1024).decode()
+            print("\33[2K",end="")
+            # print("\r"+client_ka_bheja_hua_mssg+"\nSERVER:",end=" ")
+            print(f"\r{nickname}: {client_ka_bheja_hua_mssg}\nSERVER:",end=" ")
+        except:
             break
-        print("CLIENT->",end="")
-        print(data_recv)
-        
 
-
-host = "127.0.0.1"
-port = 7000
-
+# # creating a object of socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((host,port))
+server_host = "127.0.0.1"
+server_port = 2005
+
+s.bind((server_host, server_port)) 
+
 s.listen()
-
+client_nickname = None
+print(f"SERVER is running at {server_host} on port {server_port}\n")
 conn, addr = s.accept()
-if conn:
-    conn.send("Welcome to my Server\n".encode())
-    st = threading.Thread(target=check_mssgs_from_client,args=(conn,))
-    st.start()
-else:
-    print("something went wrong while connect()")    
-    
-    
-while True:
-    if conn:
-        to_send = input("YOU(Server)-> ")
-        conn.send(to_send.encode())
-    else:
-        print("NOT CONNECTED",end="\r")     
+client_nickname = conn.recv(1024).decode()
+if client_nickname:
+    print(f"{client_nickname} has joined the server.")
+conn.send(f"Hello {client_nickname}, Welcome to the server\n".encode())
+conn.send("You can send your texts now...\n".encode())
+# Thread to handling receiving mssgs from client independently
+client_thread = threading.Thread(target=client_ne_kuch_bheja_kya, args=(conn,client_nickname))
+client_thread.start()
 
+while True:
+    d = input("\rSERVER: ")
+    conn.send(d.encode())
